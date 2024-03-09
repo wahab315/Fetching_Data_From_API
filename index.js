3dbfeba1429856d0d5c2e90d9843388d20c57bf7
wahab315 <abdulwahab315tts@gmail.com>
2024-03-09 19:18:09 +0500

    new change

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const fs = require("fs");
 const superagent = require("superagent");
 
 fs.readFile(`${__dirname}/bread.txt`, (err, data) => {
-  console.log(data);
   superagent
-    .get(`https://dog.ceo/api/breeds/${data}/image/random`)
+    .get(`https://dog.ceo/api/breeds/image/random`)
     .then((res) => {
       console.log(res.body.message);
       fs.writeFile("dogImage.txt", res.body.message, (err, mess) => {
@@ -12,6 +11,6 @@ fs.readFile(`${__dirname}/bread.txt`, (err, data) => {
       });
     })
     .catch((err) => {
-      console.log(err.message);
+      console.log(err);
     });
 });
